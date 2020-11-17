94fef00bbd70d1eb2ed920232e9d8f347e34d8ad
Dylan Connolly <dylanconnolly145@gmail.com>
2020-11-17 15:28:10 -0700

    Refactor movie image link to properly grab image from API

--- app/models/movie_overview.rb
+++ app/models/movie_overview.rb
@@ -1,10 +1,11 @@
 class MovieOverview
-    attr_reader :id, :title, :release_date, :overview
+    attr_reader :id, :title, :release_date, :overview, :backdrop_path
 
     def initialize(movie_db_data)
         @id = movie_db_data[:id]
         @title = movie_db_data[:title]
         @release_date = movie_db_data[:release_date]
         @overview = movie_db_data[:overview]
+        @backdrop_path = movie_db_data[:backdrop_path]
     end
 end
