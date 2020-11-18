c867b41766789ed29ec079f9740609fcf7ed4185
Dylan Connolly <dylanconnolly145@gmail.com>
2020-11-18 01:38:31 -0700

    Update pagination logic to display 4 next pages

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -29,8 +29,8 @@ class SearchFacade
     def pagination
         if current_page <= 5
             (1..(lesser_of_two(10, total_pages)))
-        elsif current_page > 5 && current_page < (total_pages-5)
-            ((current_page-5)..(current_page+5))
+        elsif current_page > 5 && current_page < (total_pages-4)
+            ((current_page-5)..(current_page+4))
         elsif current_page >= (total_pages-5)
             (greater_of_two(1, (total_pages-10))..total_pages)
         end
