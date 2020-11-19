1dd312bd7707d9aaee3fba91f60c74491918966d
Dylan Connolly <dylanconnolly145@gmail.com>
2020-11-19 15:50:55 -0700

    Movie show card formatting. Title and movie responsive

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie
-    attr_reader :id, :title, :director, :imdb_id, :release_date, :overview
+    attr_reader :id, :title, :director, :imdb_id, :release_date, :overview, :backdrop_path, :poster_path
 
     def initialize(movie_db_data, open_movie_db_data)
         @id = movie_db_data[:id]
@@ -8,5 +8,15 @@ class Movie
         @imdb_id = movie_db_data[:imdb_id]
         @release_date = movie_db_data[:release_date]
         @overview = movie_db_data[:overview]
+        @backdrop_path = movie_db_data[:backdrop_path]
+        @poster_path = movie_db_data[:poster_path]
+    end
+
+    def year
+        if @release_date == '' || @release_date == nil
+            ""
+        else
+            @release_date.split('-')[0]
+        end
     end
 end
